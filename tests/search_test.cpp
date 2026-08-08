608dee36c1d3c5463f061a9dd93395c44285ea35
ohhmkar <theomkargajre@gmail.com>
2026-08-08 20:47:23 +0530

    added more tests for testing bidirectional bfs

--- tests/search_test.cpp
+++ tests/search_test.cpp
@@ -79,3 +79,92 @@ TEST(Graph, FindsReverseNeighbours)
   EXPECT_EQ(
       neighbours, expected);
 }
+
+TEST(BidirectionalBFS, FindsShortestPath)
+{
+  wiki::Graph graph;
+
+  graph.addEdge("A", "B");
+  graph.addEdge("B", "C");
+  graph.addEdge("C", "D");
+
+  auto path = wiki::bidirectionalBfs(
+      "A", "D",
+      [&](const std::string &node)
+      { return graph.neighbours(node); },
+      [&](const std::string &node)
+      { return graph.reverseNeighbours(node); });
+
+  std::vector<std::string> expected = {"A", "B", "C", "D"};
+  EXPECT_EQ(path, expected);
+}
+TEST(BidirectionalBFS, ReturnsEmptyWhenNoPathExists)
+{
+  wiki::Graph graph;
+
+  graph.addEdge("A", "B");
+  graph.addEdge("C", "D");
+
+  auto path = wiki::bidirectionalBfs(
+      "A", "D",
+      [&](const std::string &node)
+      { return graph.neighbours(node); },
+      [&](const std::string &node)
+      { return graph.reverseNeighbours(node); });
+
+  EXPECT_TRUE(path.empty());
+}
+TEST(BidirectionalBFS, StartEqualsTarget)
+{
+  wiki::Graph graph;
+
+  auto path = wiki::bidirectionalBfs(
+      "A", "A",
+      [&](const std::string &node)
+      { return graph.neighbours(node); },
+      [&](const std::string &node)
+      { return graph.reverseNeighbours(node); });
+
+  std::vector<std::string> expected = {"A"};
+  EXPECT_EQ(path, expected);
+}
+TEST(BidirectionalBFS, FindsShortestPathRatherThanFirstFoundPath)
+{
+  wiki::Graph graph;
+
+  graph.addEdge("A", "B");
+  graph.addEdge("B", "C");
+  graph.addEdge("C", "D");
+  graph.addEdge("D", "E");
+  graph.addEdge("A", "E");
+
+  auto path = wiki::bidirectionalBfs(
+      "A", "E",
+      [&](const std::string &node)
+      { return graph.neighbours(node); },
+      [&](const std::string &node)
+      { return graph.reverseNeighbours(node); });
+
+  std::vector<std::string> expected = {"A", "E"};
+  EXPECT_EQ(path, expected);
+}
+
+TEST(BidirectionalBFS, MeetingDetectedDuringBackwardExpansion)
+{
+  wiki::Graph graph;
+
+  graph.addEdge("S", "A");
+  graph.addEdge("A", "B");
+  graph.addEdge("B", "C");
+  graph.addEdge("C", "T");
+
+  auto path = wiki::bidirectionalBfs(
+      "S", "T",
+      [&](const std::string &node)
+      { return graph.neighbours(node); },
+      [&](const std::string &node)
+      { return graph.reverseNeighbours(node); });
+
+  std::vector<std::string> expected = {"S", "A", "B", "C", "T"};
+  EXPECT_EQ(path, expected);
+}
